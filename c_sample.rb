ff9c676aedcd32c49d675a6747b85aee6baf7645
yutoJ <yutoogata4@gmail.com>
2018-01-14 15:46:10 +0000

    refactoring converter

--- c_sample.rb
+++ c_sample.rb
@@ -1,2 +1,10 @@
 a = Array.new(5,0)
 puts a.to_s
+
+
+def order(menu, drink: true, ice: true)
+end
+
+sample('coke',drink: true, ice: false)
+params = { drink: false, ice: false }
+sample('tea', params)
--- test/lengthConvert/ConverterTest.rb
+++ test/lengthConvert/ConverterTest.rb
@@ -4,8 +4,8 @@ require './lib/lengthConvert/Converter'
 class ConverterTest < Minitest::Test
   def test_convert_length
     conveter = Converter.new
-    assert_equal 39.37, conveter.convert_length(1, 'm', 'in')
-    assert_equal 0.38, conveter.convert_length(15, 'in', 'm')
-    assert_equal 10670.73, conveter.convert_length(35000, 'ft', 'm')
+    assert_equal 39.37, conveter.convert_length(1, from: :m, to: :in)
+    assert_equal 0.38, conveter.convert_length(15, from: :in, to: :m)
+    assert_equal 10670.73, conveter.convert_length(35000, from: :ft, to: :m)
   end
 end
