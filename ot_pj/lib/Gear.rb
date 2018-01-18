6135a063bc8814f0e78a001373209f29e707fa21
yutoJ <yutoogata4@gmail.com>
2018-01-18 16:34:32 +0000

    add new method to gear

--- ot_pj/lib/Gear.rb
+++ ot_pj/lib/Gear.rb
@@ -1,11 +1,25 @@
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
+
+
   def ratio
     ( @chainring / @cog.to_f ).floor(2)
+  end
+
+  # funny
+  def wheel
+    @rim + ( @tire * 2 )
+  end
+
+  # funny
+  def gearInch
+    wheel * ratio
   end
 end
--- ot_pj/test/BicycleTest.rb
+++ ot_pj/test/BicycleTest.rb
@@ -3,7 +3,12 @@ require './lib/Gear'
 
 class BicycleTest < Minitest::Test
   def test_calc_ratio
-    assert_equal 4.72, Gear.new(52,11).ratio
-    assert_equal 1.11, Gear.new(30,27).ratio
+    assert_equal 4.72, Gear.new(52,11, 1, 1).ratio
+    assert_equal 1.11, Gear.new(30,27, 1, 1).ratio
+  end
+
+  def test_calc_gearInch
+    assert_equal 4.72 * 3, Gear.new(52,11, 1, 1).gearInch
+    assert_equal 1.11 * 5, Gear.new(30,27, 1, 2).gearInch
   end
 end
